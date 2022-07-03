53d9e69592615c68bc431331e0086c94d0ebd0b3
Alex Aguiar <alex3aguiar@gmail.com>
2022-07-02 21:12:17 -0300

    sdsd

--- index2.js
+++ index2.js
@@ -1,15 +1,4 @@
 
-        function PIP() {
-            const video = document.getElementById("video");
-
-            const canvas = document.createElement("canvas");
-            canvas.width = video.videoWidth;
-            canvas.height = video.videoHeight;
-            // draw the video at that frame
-            canvas.getContext('2d')
-                .drawImage(video, 0, 0, 50, 50);
-
-        };
         window.onload = function () {
             document.querySelector("#video");
             navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
@@ -31,12 +20,4 @@
                 // do something
             }
 
-
-
-            setInterval(() => {
-                console.log(aovivo.checked);
-                if (aovivo.checked) {
-                    PIP();
-                }
-            }, 500);
         };
