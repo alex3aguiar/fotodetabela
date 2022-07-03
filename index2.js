e40223c70d70538a057320265ee676ae94536138
Alex Aguiar <alex3aguiar@gmail.com>
2022-07-02 21:32:06 -0300

    asdasd

--- index2.js
+++ index2.js
@@ -1,6 +1,6 @@
-
+const video = document.getElementById("video");
         window.onload = function () {
-            document.querySelector("#video");
+            
             navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
 
             if (navigator.getUserMedia) {
@@ -12,6 +12,7 @@
 
             function handleVideo(stream) {
                 video.srcObject = stream;
+                debugger;
                 video.load();
                 video.play();
             }
@@ -21,3 +22,32 @@
             }
 
         };
+        function drawToCanvas() {
+            const video = document.getElementById("video");
+            const canvas1 = document.getElementById("canvas1");
+            const canvas2 = document.getElementById("canvas2");
+            const inputCtx = canvas1.getContext("2d");
+            const outputCtx = canvas2.getContext("2d");
+            inputCtx.drawImage(video, 0, 0, width, height);
+            outputCtx.drawImage(video, 0, 0, width, height);
+            // get the pixel data from input canvas
+            const pixelData = inputCtx.getImageData(0, 0, width, height);
+            const arr = pixelData.data;
+        
+            // Iterate through every pixel, calculate x,y coordinates
+            for (let i = 0; i < arr.length; i += 4) {
+                const x = i/4 % (width);
+                const y = i / (width * 4);
+                if(((x > 85 && x < 90) || (x > 210 && x < 215)) && (y > 80)) {
+                    arr[i + 0] = 0;
+                    arr[i + 1] = 255;
+                    arr[i + 2] = 0;
+                    arr[i + 3] = 255;
+                }
+            }
+        
+            // write the manipulated pixel data to the second canvas
+            outputCtx.putImageData(pixelData, 0, 0);
+            // recurse to itself for every animation frame
+            requestAnimationFrame(drawToCanvas);
+        }        
